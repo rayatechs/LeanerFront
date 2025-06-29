4bc943ea46fccf38b920b853058e8914520f5be0
Hossein <43233476+Nothehi@users.noreply.github.com>
2025-06-29 18:36:13 +0000

    feat: Add dark mode and theme switch

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import ThemeToggle from "@/components/theme-toggle";
 
 export default function Home() {
   return (
@@ -49,6 +50,8 @@ export default function Home() {
           >
             Read our docs
           </a>
+          
+          <ThemeToggle />
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
