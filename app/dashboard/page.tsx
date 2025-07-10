40d4b280165b01561bd4a7c5ca052ecff128027b
Hossein <43233476+Nothehi@users.noreply.github.com>
2025-07-10 06:40:45 +0000

    feat: improve responsive layout for header and sidebar components

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -13,15 +13,15 @@ export default function Page() {
       <SidebarProvider className="flex flex-col">
         <header className="sticky top-0 z-50 flex w-full items-center bg-navbar">
           <div className="flex h-(--header-height) w-full items-center gap-2 px-4 py-2">
-            <div className="w-1/3">
+            <div className="md:w-1/3">
               <span className="bg-primary/10 text-primary flex h-8 w-8 items-center justify-center rounded-lg">
                 <Hourglass width={16} />
               </span>
             </div>
-            <div className="w-1/3">
+            <div className="md:w-1/3 flex-1 md:flex-auto">
               <SearchForm className="w-1/2 mx-auto" />
             </div>
-            <div className="w-1/3">
+            <div className="w-1/3 hidden md:block">
 
             </div>
           </div>
@@ -29,7 +29,7 @@ export default function Page() {
         
         <div className="flex flex-row w-full h-full">
           <AppSidebar side="right" collapsible="icon" />
-          <SidebarInset className="!m-0 !rounded-l-none !rounded-b-none fixed inset-y-0 top-(--header-height) h-[calc(100svh-var(--header-height)-calc(var(--spacing) * 2))]! w-[calc(100%-var(--sidebar-width))]! left-0 overflow-y-auto peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-20px)]! transition-[left,right,width] duration-200 ease-linear">
+          <SidebarInset className="!m-0 !rounded-l-none !rounded-b-none fixed inset-y-0 top-(--header-height) h-[calc(100svh-var(--header-height)-calc(var(--spacing) * 2))]! md:w-[calc(100%-var(--sidebar-width))] w-full left-0 overflow-y-auto peer-data-[state=collapsed]:w-[calc(100%-var(--sidebar-width-icon)-20px)]! transition-[left,right,width] duration-200 ease-linear">
             <SiteHeader />
             <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
               <div className="grid auto-rows-min gap-4 md:grid-cols-3">
